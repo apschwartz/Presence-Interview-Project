9d3a4d5e2dc59088bd0487ae4eef7302b3cf3e2c
Andy Schwartz <andy-github@schegg.org>
2018-11-16 11:27:29 -0800

    Make use of Promise for server-delete call

--- force-app/main/default/aura/ContactPaymentsTable/ContactPaymentsTableHelper.js
+++ force-app/main/default/aura/ContactPaymentsTable/ContactPaymentsTableHelper.js
@@ -9,22 +9,35 @@
         toastEvent.fire();
     },
 
+    // This should become a static resource function.  
+    doApexCall: function (action) {
+        return new Promise(function (resolve, reject) {
+            action.setCallback(this, function (response) {
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    resolve(response.getReturnValue());
+                }
+                else if (state === "ERROR") {
+                    reject(response.getError());
+                 }
+            });
+            $A.enqueueAction(action);
+        });
+    },
 
-    deleteRecord: function (component, paymentId) {
+    deleteRecord: function (component, paymentId, helper) {
         var action = component.get("c.deletePayment");
         action.setParams({ paymentId: paymentId });
-        action.setCallback(this, function (actionResult) {
-            console.log(JSON.stringify(actionResult.getState()));
-            if (actionResult.getState() === 'SUCCESS') {
-                this.doToast('Payment Deleted');
-            }
-            else {
-                this.doToast('Payment Delete Failed: ' + actionResult.getError()[0].pageErrors[0].message);
-            }
-            var paymentUpdate = component.getEvent("paymentChangeEvent");
-            paymentUpdate.fire();
-        });
-        $A.enqueueAction(action);
+        this.doApexCall(action)
+            .then($A.getCallback(function (result) {
+                helper.doToast('Payment Deleted');
+            })).catch($A.getCallback(function (errors) {
+                helper.doToast('Payment Delete failed:' + errors[0].pageErrors[0].message);
+            })).then($A.getCallback(function (result) {
+                // always executed after Then or Catch above.
+                var paymentUpdate = component.getEvent("paymentChangeEvent");
+                paymentUpdate.fire();
+            }));
     }
 
 })
